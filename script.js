65bcf7ed0ab38f48566b99e0b8188ed7a253b399
afq0260-boop <afq.0260@gmail.com>
2026-05-02 12:44:47 +0300

    Update API endpoint in send function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function send() {
   const message = document.getElementById("msg").value;
 
-  const res = await fetch("https://q-server-2.onrender.com/api/chat", {
+  const res = await fetch("https://q-2-4ryw.onrender.com/api/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
